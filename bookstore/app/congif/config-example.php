2a2d394a021487b654f2ce549183471e466cb2f4
Marko <markofly2005@gmail.com>
2026-04-25 12:59:15 +0200

    .

---
+++ bookstore/app/congif/config-example.php
@@ -0,0 +1,8 @@
+<?php
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASSWORD = '';
+const DATABASE = 'bookstore';
+const PORT = 3306;
+const BASE_URL = 'http://localhost/bookstore/public/';
